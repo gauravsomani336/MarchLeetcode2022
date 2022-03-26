d865651e6aafad69a7afbe00f4d47d5fa657e468
Gaurav somani <40043226+gauravsomani336@users.noreply.github.com>
2022-03-27 00:25:53 +0530

    Time: 3 ms (83.95%), Space: 6.6 MB (59.91%) - LeetHub

--- 763-partition-labels/763-partition-labels.cpp
+++ 763-partition-labels/763-partition-labels.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
-        vector<int>lastOccurence(26,0);
+        vector<int>l(26,0);
         vector<int>res;
-        for(int i=0;i<s.size();i++)
+        int n=s.size();
+        for(int i=0;i<n;i++)
         {
-            lastOccurence[s[i]-'a']=i;
+            l[s[i]-'a']=i;
         }
+        
         int start=0;
-        int last=0;
-        for(int i=0;i<s.size();i++)
+       int last=INT_MIN;
+        for(int i=0;i<n;i++)
         {
-            last=max(last,lastOccurence[s[i]-'a']);
-            if(last==i)
-            {
-                res.push_back((last-start)+1);
-                start=i+1;
-            }
+            last=max(last, l[s[i]-'a']);
+                if(last==i)
+                {
+                    res.push_back((last-start)+1);
+                    start=last+1;
+                }
         }
         return res;
+        
         
     }
 };
